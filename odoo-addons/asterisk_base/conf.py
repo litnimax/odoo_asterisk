5352fb49d85997f692a256dceb61f21ac826cfa6
litnimax <litnimax@pbxware.ru>
2017-05-06 03:39:20 +0300

    Show filename on the same tab on config create

--- odoo-addons/asterisk_base/conf.py
+++ odoo-addons/asterisk_base/conf.py
@@ -7,4 +7,11 @@ class Conf(models.Model):
     _recname = 'filename'
 
     filename = fields.Char(required=True)
+    # Trick to show filename above content when creating new file
+    filename_on_create = fields.Char()
     content = fields.Text()
+
+
+    @api.onchange("filename_on_create")
+    def _onchange_filename_on_create(self):
+        self.filename = self.filename_on_create
