a9593c2fd2c188d36dca6337de7c52436f13060f
litnimax <litnimax@pbxware.ru>
2017-05-16 17:40:13 +0300

    Agent small rework.

--- agent/asterisk_helper.py
+++ agent/asterisk_helper.py
@@ -9,21 +9,7 @@ from tornado.httpserver import HTTPServer
 logging.basicConfig(level=logging.DEBUG)
 _logger = logging.getLogger(__name__)
 
-
-PORT = 8010
-ASTERISK = '/usr/sbin/asterisk'
-ASTERISK_ARGS = '-cr'
-ASTERISK_RECORDING_FOLDER = '/var/spool/asterisk/monitor'
-
-SSL_ENABLED = False
-SSL_CERT = './cert.pem'
-SSL_KEY = './privkey.pem'
-
-try:
-    from local_conf import *
-except ImportError:
-    _logger.warning('No local_conf.py found or import error!')
-
+from conf import *
 
 class MyTermSocket(TermSocket):
 
